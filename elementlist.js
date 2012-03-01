5730288c66e88f721c78e41a36380d272468eb5f
Ondrej Nespor <ondrej.nespor@slevomat.cz>
2012-03-01 16:22:04 +0100

    Version 1.0 documentation

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Slevomat_Client_Partner"],["c","Slevomat_Client_Response_Abstract"],["c","Slevomat_Client_Response_Failure"],["c","Slevomat_Client_Response_Success"],["c","Slevomat_Client_ResponseFactory"]];
